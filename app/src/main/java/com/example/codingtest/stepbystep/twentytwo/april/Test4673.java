a027af9abde7b6c7bc19d38524711b92747bd6b5
souyeolBaek <merrygoaround0726@gmail.com>
2022-04-26 00:03:31 +0900

    1065 풀이중..

--- app/src/main/java/com/example/codingtest/stepbystep/twentytwo/april/Test4673.java
+++ app/src/main/java/com/example/codingtest/stepbystep/twentytwo/april/Test4673.java
@@ -1,52 +1,43 @@
 package com.example.codingtest.stepbystep.twentytwo.april;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.IntStream;
 
 public class Test4673 {
+
     public static void main(String[] args) {
-        selfNum();
-    }
 
-    public static void selfNum() {
-        int n = 1; //시작 값
-        List<Integer> list = new ArrayList<>();
-        List<Integer> result = new ArrayList<>();
-        while (n <= 10000) {
-            int value;
-            
-            int length = String.valueOf(n).length();
+        boolean[] check = new boolean[10001];
 
-            if (n < 10) {
-                value = n + n;
-                System.out.println(value);
-            } else {
-                value = forFun(n, length);
+        for (int i = 1; i < 10001; i++) {
+            int self = getSelf(i);
 
+            //true는 셀프넘버가 아닌 숫자이다.
+            if (self < 10001)
+                check[self] = true;
+
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        //false인 값만 출렧한다.
+        for (int i = 1; i < 10001; i++) {
+            if (!check[i]) {
+                sb.append(i).append('\n');
             }
-            list.add(value);
-            n++;
         }
 
-        IntStream.range(0, 10000)
-                .filter(v -> !list.contains(v))
-                .forEach(i -> result.add(i));
-
-//        result.forEach(System.out::println);
+        System.out.print(sb);
     }
 
-    private static int forFun(int n, int cnt) {
-        int sum = 0;
-        for (int i = 0; i < cnt; i++) {
-            sum += parseInt(n, i, i + 1);
+
+    //셀프넘버를 구한다. 1에서 10000까지 포함되지 않는 숫자가 셀프넘버이다.
+    public static int getSelf(int num) {
+        int total = num;
+        while (num != 0) { //0이 될때 까지 돌린다.
+            total += (num % 10);
+            num /= 10;
         }
-        return sum;
+        return total;
     }
 
-    private static int parseInt(int n, int start, int end) {
-        return Integer.parseInt(String.valueOf(n).substring(start, end));
-    }
 }
 
-// TODO: 2022/04/22 이건 답안 보자.. 너무 어렵다ㅣ.. 
