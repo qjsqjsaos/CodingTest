96831680a26bf6c3f65e18c4a5b9f9a15ab834bc
SooYeol Baek <merrygoaround0726@gmail.com>
2022-09-12 20:56:36 +0900

    18870 문제풀이 중

---
+++ app/src/main/java/com/example/codingtest/stepbystep/twentytwo/september/Test18870.java
@@ -0,0 +1,12 @@
+package com.example.codingtest.stepbystep.twentytwo.september;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Test18870 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
